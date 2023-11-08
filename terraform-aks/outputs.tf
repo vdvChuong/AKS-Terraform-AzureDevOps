63bf18d1e611715da4451d7b1dbb69c6958a9243
Dinh Vinh Chuong Vo <chuongdinhvinh.vo2@dxc.com>
2023-11-08 14:14:55 +0700

    trigger pipeline

--- terraform-aks/outputs.tf
+++ terraform-aks/outputs.tf
@@ -5,5 +5,5 @@ output "name" {
 
 output "aks_cluster_version" {
   value       = azurerm_kubernetes_cluster.aks_cluster.kubernetes_version
-  description = "Specifies the version of the AKS cluster."
+  description = "Specifies the version of the AKS cluster ."
 }
