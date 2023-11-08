3d2830d3c2b7bbb6efb7f6e190f7de927678ef60
Dinh Vinh Chuong Vo <chuongdinhvinh.vo2@dxc.com>
2023-11-08 14:13:53 +0700

    add output file

--- terraform-aks/aks-cluster.tf
+++ terraform-aks/aks-cluster.tf
@@ -1,4 +1,4 @@
-resource "azurerm_kubernetes_cluster" "example" {
+resource "azurerm_kubernetes_cluster" "aks_cluster" {
   name                    = var.aks_cluster_name
   location                = var.location
   resource_group_name     = azurerm_resource_group.aks_rg.name
---
+++ terraform-aks/outputs.tf
@@ -0,0 +1,9 @@
+output "name" {
+  value       = azurerm_kubernetes_cluster.aks_cluster.name
+  description = "Specifies the name of the AKS cluster."
+}
+
+output "aks_cluster_version" {
+  value       = azurerm_kubernetes_cluster.aks_cluster.kubernetes_version
+  description = "Specifies the version of the AKS cluster."
+}
