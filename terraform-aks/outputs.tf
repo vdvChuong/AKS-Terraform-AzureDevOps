7e9e5a212fd7609331d88f6fa81403a6d2d4a0ff
Dinh Vinh Chuong Vo <chuongdinhvinh.vo2@dxc.com>
2023-12-05 00:16:59 +0700

    fix output

--- terraform-aks/outputs.tf
+++ terraform-aks/outputs.tf
@@ -1,9 +1,9 @@
 output "name" {
-  value       = azurerm_kubernetes_cluster.aks_cluster.name
+  value       = azurerm_kubernetes_cluster.aks.name
   description = "Specifies the name of the AKS cluster."
 }
 
 output "aks_cluster_version" {
-  value       = azurerm_kubernetes_cluster.aks_cluster.kubernetes_version
+  value       = azurerm_kubernetes_cluster.aks.kubernetes_version
   description = "Specifies the version of the AKS cluster."
 }
